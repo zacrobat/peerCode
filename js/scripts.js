7de4a615ba492ea9cd1d8db160689cf5ca2ffacf
Zac Cobb <zacrobat55@gmail.com>
2015-11-11 20:47:19 +1100

    jquery .insertAfer() now in use

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,21 @@ console.log("Hello World");
 $( document ).ready(function() {
    // ALL your JS logic here
 
-// var logic = [
-// 	"Do you completely understand the logic by the code without the comments:", 
-// 	"If not is there comments to indicate the logic:"
-// 	];
+$( "<p>I'm from the javascript page</p>" ).insertAfter( ".Lil_Wayne" );
 
-// var tested = [
-// 	"Do the test(s) cover all the logic within the method?:",
-// 	"For any new method can it be tested:"
-// 	];
-document.getElementById("thisDiv").innerHTML = "Output from scripts.js";
+
+var logic = [
+	"Do you completely understand the logic by the code without the comments:", 
+	"If not is there comments to indicate the logic:"
+	];
+
+var tested = [
+	"Do the test(s) cover all the logic within the method?:",
+	"For any new method can it be tested:"
+	];
+
+
 
 });
-
 
 
