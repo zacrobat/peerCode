b3b13206447476783696c59b2e8fe8979dc997da
Zac Cobb <zacrobat55@gmail.com>
2015-11-11 11:27:13 +1100

    initial setup

--- js/jquery.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-   // ALL your JS logic here
-
-});
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-scripts.js
+console.log("Hello World");
+
+$( document ).ready(function() {
+   // ALL your JS logic here
+
+// var logic = [
+// 	"Do you completely understand the logic by the code without the comments:", 
+// 	"If not is there comments to indicate the logic:"
+// 	];
+
+// var tested = [
+// 	"Do the test(s) cover all the logic within the method?:",
+// 	"For any new method can it be tested:"
+// 	];
+document.getElementById("thisDiv").innerHTML = "Output from scripts.js";
+
+});
+
+
+
